475fc88e1a0cbe22bf6ad5e44eff26731b604941
Dusttoo <dusty.mumphrey@yahoo.com>
2022-10-11 18:02:59 -0500

    styling recipe quiz

--- web/components/ingredientQuestions.js
+++ web/components/ingredientQuestions.js
@@ -6,26 +6,29 @@ import { RecipeResults } from './recipeResults';
 export const IngredientQuestions = ({user}) => {
     const [questionNumber, setQuestionNumber] = useState(1)
     const [answers, setAnswers] = useState([])
+    const [results, setResults] = useState([])
     const router = useRouter()
     const handleNext = () => {
         setQuestionNumber(questionNumber + 1)
     }
 
     const updateOptions = (e) => {
-        console.log(answerResponses[questionNumber]['answer'])
-        answerResponses[questionNumber]['answer'].push(e.target.value)
-        console.log(answerResponses[questionNumber]['answer'])
+        if(answerResponses[questionNumber]['answer'].includes(e.target.value)) {
+            answerResponses[questionNumber]['answer'].splice(answerResponses[questionNumber]['answer'].indexOf(e.target.value), 1)
+        } else {
+            answerResponses[questionNumber]['answer'].push(e.target.value)
+        }
     }
+
     const calculateResults = async () => {
         // This function makes the call to google crawl
         // Then sets answers to the response
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recipes/search?search_params=toast`);
         const json = await res.json()
-        console.log(json)
-        setAnswers(json)
-        console.log(answers)
+        setResults(json)
+        setAnswers(answerResponses)
     }
-    console.log(answers)
+
     return (
         
         <div className='bg-white h-auto p-4 rounded text-center '>
@@ -61,20 +64,20 @@ export const IngredientQuestions = ({user}) => {
                     {questions[questionNumber + 1] ? 
                     <button
                     type='button'
-                    className=""
+                    className="bg-main-1 m-4 text-white px-4 rounded-lg hover:text-main-1 hover:bg-white"
                     onClick={() => handleNext()}
                     >
                     Next
                     </button> : 
                     <button
                     onClick={() => calculateResults()}
-                    className=""
+                    className="bg-main-1 m-4 text-white px-4 rounded-lg hover:text-main-1 hover:bg-white"
                     >
                     Get your results
                     </button>
                     }
                 </> : 
-                <RecipeResults answers={answers}/>
+                <RecipeResults answers={results}/>
                 }
                 </>
                  :
@@ -83,12 +86,12 @@ export const IngredientQuestions = ({user}) => {
                         <a>Please log in</a>
                     </Link>
                  </>
-                // <h2>Please log in</h2>
     }
            
         </div>
     )
   }
+
 
 const questions = {
     1: {'Question': 'What proteins do you have?',
@@ -98,19 +101,19 @@ const questions = {
             {'option': 'c', 'choice' : 'Fish'},
             {'option': 'd', 'choice' : 'Beef'}
         ]},
-    2: {'Question': 'text',
+    2: {'Question': 'What vegetables do you have?',
         'Options': [
-            {'option': 'a', 'choice' : 'choice'},
-            {'option': 'b', 'choice' : 'choice'},
-            {'option': 'c', 'choice' : 'choice'},
-            {'option': 'd', 'choice' : 'choice'}
+            {'option': 'a', 'choice' : 'Corn'},
+            {'option': 'b', 'choice' : 'Green Beans'},
+            {'option': 'c', 'choice' : 'Mashed Potaatoes'},
+            {'option': 'd', 'choice' : 'Carrots'}
         ]},
-    3: {'Question': 'text',
+    3: {'Question': 'What condiments do you have?',
         'Options': [
-            {'option': 'a', 'choice' : 'choice'},
-            {'option': 'b', 'choice' : 'choice'},
-            {'option': 'c', 'choice' : 'choice'},
-            {'option': 'd', 'choice' : 'choice'}
+            {'option': 'a', 'choice' : 'Ketchup'},
+            {'option': 'b', 'choice' : 'Mayonaise'},
+            {'option': 'c', 'choice' : 'Mustard'},
+            {'option': 'd', 'choice' : 'BBQ Sauce'}
         ]}
 }
 
